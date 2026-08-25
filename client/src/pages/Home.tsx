46921d0ea283756f296f90ceb13d266f81d9c2ad
Khaleesisaithe <1.24489566e+08+Khaleesisaithe@users.noreply.github.com>
2026-08-25 08:30:02 +0000

    fix: restore PixBee brand assets

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3595,7 +3595,7 @@ function CreatorAboutPage() {
         <article className="glass-panel creator-profile">
           <div className="creator-photo-frame">
             <img
-              src="/manus-storage/khaleesi-saithe-profile_08105716.jpg"
+              src="/assets/khaleesi-saithe-profile.png"
               alt={locale === "en" ? "Khaleesi Saithe, creator of PixBee FechaCaixa" : "Khaleesi Saithe, criadora do PixBee FechaCaixa"}
             />
           </div>
